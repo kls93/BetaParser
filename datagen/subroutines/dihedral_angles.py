c98564d01c8c1ab3a304127c6bbd5c2dd1c2fc3d
kls93 <kathryn.l.shelley@gmail.com>
2018-03-11 15:16:27 +0000

    Fixes bug with DSSP file phi and psi angle calculation (now calculates backbone and side chain torsion angles using ISAMBARD instead)

---
+++ datagen/subroutines/dihedral_angles.py
@@ -0,0 +1,139 @@
+
+import pandas as pd
+import isambard
+from collections import OrderedDict
+if __name__ == 'subroutines.dihedral_angles':
+    from subroutines.run_stages import run_stages
+else:
+    from datagen.subroutines.run_stages import run_stages
+
+
+class backbone_torsion_angles():
+
+    def __init__(self, pdb):
+        self.pdb = pdb
+
+    def calc_omega_phi_psi_angles(self, domain_id, dssp_df):
+        # Calculates omega, phi and psi dihedral angles for each residue in
+        # beta_structure
+        print('Calculating backbone torsion angles in {}'.format(domain_id))
+
+        omega = ['']*dssp_df.shape[0]
+        phi = ['']*dssp_df.shape[0]
+        psi = ['']*dssp_df.shape[0]
+
+        res_ids = dssp_df['RES_ID'].tolist()
+        res_ids_filtered = []
+        for res_id in res_ids:
+            if res_id not in res_ids_filtered:
+                res_ids_filtered.append(res_id)
+        res_ids = res_ids_filtered
+
+        residues = list(self.pdb.get_monomers())
+        angles = isambard.ampal.analyse_protein.measure_torsion_angles(residues)
+        angles = OrderedDict(zip(res_ids, angles))
+
+        for row in range(dssp_df.shape[0]):
+            if (
+                dssp_df['RES_ID'][row] in list(angles.keys())
+                and dssp_df['ATMNAME'][row] == 'CA'
+            ):
+                omega_val = angles[dssp_df['RES_ID'][row]][0]
+                if type(omega_val) == float:
+                    omega_val = round(omega_val, 1)
+
+                phi_val = angles[dssp_df['RES_ID'][row]][1]
+                if type(phi_val) == float:
+                    phi_val = round(phi_val, 1)
+
+                psi_val = angles[dssp_df['RES_ID'][row]][2]
+                if type(psi_val) == float:
+                    psi_val = round(psi_val, 1)
+
+                omega[row] = omega_val
+                phi[row] = phi_val
+                psi[row] = psi_val
+
+        angle_df = pd.DataFrame({'PHI': phi,
+                                 'PSI': psi,
+                                 'OMEGA': omega})
+
+        dssp_df = pd.concat([dssp_df, angle_df], axis=1)
+
+        return dssp_df
+
+
+class side_chain_torsion_angles():
+
+    def __init__(self, pdb):
+        self.pdb = pdb
+
+    def calc_chi_angles(self, domain_id, dssp_df):
+        # Calculates side chain torsion angles for each residue in
+        # beta_structure
+        print('Calculating side chain torsion angles in {}'.format(domain_id))
+
+        chi = ['']*dssp_df.shape[0]
+
+        res_ids = dssp_df['RES_ID'].tolist()
+        res_ids_filtered = []
+        for res_id in res_ids:
+            if res_id not in res_ids_filtered:
+                res_ids_filtered.append(res_id)
+        res_ids = res_ids_filtered
+        chi_angles = []
+
+        residues = list(self.pdb.get_monomers())
+        for res in residues:
+            angles = isambard.ampal.analyse_protein.measure_sidechain_torsion_angles(
+                res
+            )
+            angles = [round(angle, 1) for angle in angles if type(angle) == float]
+            chi_angles.append(angles)
+        chi_angles = OrderedDict(zip(res_ids, chi_angles))
+
+        for row in range(dssp_df.shape[0]):
+            if (
+                dssp_df['RES_ID'][row] in list(chi_angles.keys())
+                and dssp_df['ATMNAME'][row] == 'CA'
+            ):
+                chi[row] = chi_angles[dssp_df['RES_ID'][row]]
+
+        angle_df = pd.DataFrame({'CHI_ANGLES': chi})
+        dssp_df = pd.concat([dssp_df, angle_df], axis=1)
+
+        return dssp_df
+
+
+class backbone_geometry():
+    # Maybe calculate rise per rsidue, residues per turn and radius of helix
+
+    def __init__(self, pdb):
+        self.pdb = pdb
+
+
+class dihedral_angles(run_stages):
+
+    def __init__(self, run_parameters):
+        run_stages.__init__(self, run_parameters)
+
+    def calc_dihedral_angles(self, sec_struct_dfs_dict):
+        for domain_id in list(sec_struct_dfs_dict.keys()):
+            dssp_df = sec_struct_dfs_dict[domain_id]
+
+            # Creates AMPAL object
+            pdb_file_lines = ''.join(dssp_df['PDB_FILE_LINES'].tolist())
+            pdb = isambard.ampal.convert_pdb_to_ampal(
+                'Beta_strands/{}.pdb'.format(domain_id)
+            )
+
+            # Measures backbone torsion angles
+            beta_structure = backbone_torsion_angles(pdb)
+            dssp_df = beta_structure.calc_omega_phi_psi_angles(domain_id, dssp_df)
+
+            beta_structure = side_chain_torsion_angles(pdb)
+            dssp_df = beta_structure.calc_chi_angles(domain_id, dssp_df)
+
+            sec_struct_dfs_dict[domain_id] = dssp_df
+
+        return sec_struct_dfs_dict
